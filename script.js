c69def784979ec31836e0bf1fcba247a343cc27b
tejbari5 <136843098+tejbari5@users.noreply.github.com>
2023-08-20 17:24:57 +0530

    first commit

--- script.js
+++ script.js
@@ -3,54 +3,98 @@ const formButton = document.getElementById("formbtn");
 const tableContainer = document.getElementById("table");
 const search = document.getElementById("search-box");
 let formmode = 0;
-let editIndex = -1;
 let studentData = JSON.parse(localStorage.getItem("studentData") ?? "[]");
+let studentCount =
+  localStorage.getItem("studentCount") ?? Math.round(Math.random() * 1000000);
 
 // functions
 // adding new students on click
 function addStudent() {
-  const data = new FormData(form);
   let newstudent = {
-    name: "no name",
-    email: "no@email.com",
-    gpa: "0",
-    age: "16",
-    degree: "BTech",
+    _id: ++studentCount,
+    name: "",
+    email: "",
+    gpa: "",
+    age: "",
+    degree: "",
   };
-  data.forEach((v, k) => (newstudent[k] = v ? v : newstudent[k]));
-  studentData = [...studentData, newstudent];
+  for (const key in newstudent)
+    if (form[key].type !== "hidden") newstudent[key] = form[key].value;
+  studentData.push(newstudent);
   localStorage.setItem("studentData", JSON.stringify(studentData));
+  localStorage.setItem("studentCount", studentCount);
   form.reset();
   display(studentData);
 }
 
 // handling click ev for edit of student
-function handleStudentEdit(i) {
-  editIndex = i;
+function handleStudentEdit(id) {
   formmode = 1;
+  edit_id.innerText = `(Editing Student ID: ${id})`;
   formButton.value = "Edit Student";
-  form.name.value = studentData[i].name;
-  form.email.value = studentData[i].email;
-  form.gpa.value = studentData[i].gpa;
-  form.age.value = studentData[i].age;
-  form.degree.value = studentData[i].degree;
+  const stData = studentData.find((student) => student._id === id);
+  for (const key in stData) form[key].value = stData[key];
 }
 
 // editing student
 function editStudent() {
-  const data = new FormData(form);
-  data.forEach((v, k) => (studentData[editIndex][k] = v));
+  let stData = studentData.find(
+    (student) => student._id === Number(form._id.value)
+  );
+  for (const key in stData)
+    if (form[key].type !== "hidden") stData[key] = form[key].value;
   localStorage.setItem("studentData", JSON.stringify(studentData));
   display(studentData);
-  editIndex = -1;
   handleReset();
 }
 
+// autofill student details
+function handleAutofill() {
+  const degrees = [
+    "AA",
+    "AAA",
+    "AE",
+    "AS",
+    "AGS",
+    "ASN",
+    "AF",
+    "BArch",
+    "BA",
+    "AB",
+    "BS",
+    "BSc",
+    "SB",
+    "ScB",
+    "BAA",
+    "BEng",
+    "BE",
+    "BSE",
+    "BESc",
+    "BSEng",
+    "BASc",
+    "BTech",
+    "BSc(Eng)",
+    "AMIE",
+    "GradIETE",
+  ];
+  let newstudent = {
+    name: generateRandomName(),
+    email: generateRandomName("email"),
+    gpa: generateRandom(0, 10),
+    age: generateRandom(1, 50),
+    degree: degrees[generateRandom(0, degrees.length - 1)],
+  };
+  for (const key in newstudent) form[key].value = newstudent[key];
+}
+
 // deleting student
-function handleStudentDelete(i) {
-  studentData = studentData.filter((_, index) => index !== i);
-  localStorage.setItem("studentData", JSON.stringify(studentData));
-  display(studentData);
+function handleStudentDelete(id) {
+  if (confirm("Delete this student?")) {
+    studentData = studentData.filter((student) => student._id !== id);
+    localStorage.setItem("studentData", JSON.stringify(studentData));
+    display(studentData);
+    handleReset();
+  }
 }
 
 // displaying the student data from the params array
@@ -66,25 +110,28 @@ function display(data) {
                 <td colspan="5">${student.email}</td>
                 <td colspan="2">${student.age}</td>
                 <td colspan="2">${student.gpa}</td>
-                <td colspan="3"><div class="cell-block">
-                ${student.degree}
-                <div>
-                <img src="edit.svg" height="16" onclick="handleStudentEdit(${i})"></img>
-                <img src="trash.svg" height="16" onclick="handleStudentDelete(${i})"></img>
-                </div>
-                </div></td>
+                <td colspan="3">${student.degree}</td>
+                <td>
+                  <img src="edit.svg" height="16" onclick="handleStudentEdit(${
+                    student._id
+                  })"></img>
+                  <img src="trash.svg" height="16" onclick="handleStudentDelete(${
+                    student._id
+                  })"></img>
+                </td>
                 `;
       newTbody.appendChild(row);
     });
   else {
     const row = document.createElement("tr");
     row.innerHTML = `
-                <td colspan="1" role="row"> </td>
-                <td colspan="5"> </td>
-                <td colspan="5"> </td>
-                <td colspan="2"> </td>
-                <td colspan="2"> </td>
-                <td colspan="3"> </td>
+                <td colspan="1" role="row">--</td>
+                <td colspan="5">--</td>
+                <td colspan="5">--</td>
+                <td colspan="2">--</td>
+                <td colspan="2">--</td>
+                <td colspan="3">--</td>
+                <td>--</td>
                 `;
     newTbody.appendChild(row);
   }
@@ -100,25 +147,49 @@ function handleSearch() {
     let newData = [...studentData];
     newData = newData.filter(
       (student) =>
-        regex.test(student.name.degree) ||
+        regex.test(student.name) ||
         regex.test(student.email) ||
+        regex.test(student.age) ||
+        regex.test(student.gpa) ||
         regex.test(student.degree)
     );
     display(newData);
   }
 }
 
-// handling what to do when form is reset
+// handle what to do when form is reset
 function handleReset() {
   formmode = 0;
+  edit_id.innerText = "";
   formButton.value = "Add Student";
   form.reset();
+}
+
+// generates random number between given limit
+function generateRandom(start = 1, stop = 100) {
+  return Math.round(Math.random() * (stop - start) + start);
+}
+
+// generates random name
+function generateRandomName(type = "name") {
+  const vowels = "aeiou";
+  const consonants = "bcdfghjklmnpqrstvwxyz";
+  const nameLength = Math.random(Math.random() * 4) + 4;
+  let name = "";
+  let useVowel = Math.random() < 0.5;
+  for (let i = 0; i < nameLength; i++) {
+    if (useVowel) name += vowels[generateRandom(0, 4)];
+    else name += consonants[generateRandom(0, 20)];
+    useVowel = !useVowel;
+  }
+  return type === "name" ? name : name + "@gmail.com";
 }
 
 // main logic
 document.onload = display(studentData);
 search.addEventListener("keyup", handleSearch);
-form.addEventListener("reset", handleReset);
-formButton.addEventListener("click", () =>
-  formmode === 0 ? addStudent() : editStudent()
-);
+form.onsubmit = () => {
+  formmode === 0 ? addStudent() : editStudent();
+  // return true will call for action and reload the page
+  return false;
+};
